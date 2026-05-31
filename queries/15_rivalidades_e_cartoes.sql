8f295504f362c1849693e65063f41507721c250e
Bruno Hollyver Alves dos Mártires <bruno.martires@arapiraca.ufal.br>
2026-05-31 18:01:13 -0300

    Add support for new card types in SQL query

--- queries/15_rivalidades_e_cartoes.sql
+++ queries/15_rivalidades_e_cartoes.sql
@@ -4,7 +4,7 @@ WITH EventosFiltrados AS (
         Event_Type, 
         Minute
     FROM tb_events
-    WHERE Event_Type IN ('Y', 'R')
+    WHERE Event_Type IN ('Y', 'R', 'RSY')
     GROUP BY id_match_player, Event_Type, Minute
 ),
 
@@ -12,7 +12,8 @@ CartoesPorPartida AS (
     SELECT 
         mp.MatchID AS [MatchID],
         SUM(CASE WHEN ef.Event_Type = 'Y' THEN 1 ELSE 0 END) AS [Amarelos_Jogo],
-        SUM(CASE WHEN ef.Event_Type = 'R' THEN 1 ELSE 0 END) AS [Vermelhos_Jogo],
+        SUM(CASE WHEN ef.Event_Type = 'R' THEN 1 ELSE 0 END) AS [Vermelhos_Diretos],
+        SUM(CASE WHEN ef.Event_Type = 'RSY' THEN 1 ELSE 0 END) AS [Vermelhos_Por_Amarelo],
         COUNT(*) AS [Total_Cartoes_Jogo]
     FROM EventosFiltrados AS ef
     INNER JOIN tb_match_players AS mp ON ef.id_match_player = mp.id_match_player
@@ -23,7 +24,8 @@ ConfrontosEstruturados AS (
     SELECT 
         cp.[MatchID],
         cp.[Amarelos_Jogo],
-        cp.[Vermelhos_Jogo],
+        cp.[Vermelhos_Diretos],
+        cp.[Vermelhos_Por_Amarelo],
         cp.[Total_Cartoes_Jogo],
         r.Year AS [Ano_Copa],
         CASE WHEN t1.Team_Name < t2.Team_Name THEN t1.Team_Name ELSE t2.Team_Name END AS [Selecao_1],
@@ -40,8 +42,9 @@ SELECT
     [Selecao_2] AS [Seleção B],
     COUNT(DISTINCT [MatchID]) AS [Total de Jogos],
     GROUP_CONCAT(DISTINCT [Ano_Copa] ORDER BY [Ano_Copa] ASC) AS [Anos_Das_Copas], 
-    SUM([Amarelos_Jogo]) AS [Total Amarelos],
-    SUM([Vermelhos_Jogo]) AS [Total Vermelhos],
+    SUM([Amarelos_Jogo]) AS [Amarelos],
+    SUM([Vermelhos_Diretos]) AS [Vermelhos Diretos],
+    SUM([Vermelhos_Por_Amarelo]) AS [Vermelho por Dois Amarelos],
     SUM([Total_Cartoes_Jogo]) AS [Total Acumulado de Cartões]
 FROM ConfrontosEstruturados
 GROUP BY [Selecao_1], [Selecao_2]
